2a1d1b515c80e22d2ea8ba3cdb2e61ee175e0ddf
vignesh1Annam <vigneshgoudvigneshgoud125@gmail.com>
2025-05-02 22:21:27 +0530

    Add files via upload

---
+++ GREATEST NUMBER DIVIASOR.py
@@ -0,0 +1,11 @@
+def find_gcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+# Example usage
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+gcd = find_gcd(num1, num2)
+print(f"The GCD of {num1} and {num2} is {gcd}.")
